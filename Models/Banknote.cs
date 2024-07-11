8a1544359815b70dc52c312d9cc358b3a5606bd5
SIrem Alkan <alkanirem212@gmail.com>
2024-07-11 10:01:31 +0300

    ::

--- Models/Banknote.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ATMWithdrawalApi.Models
-{
-    public class Banknote
-    {
-        public int Id { get; set; }
-        public int Type { get; set; }
-        public int Count { get; set; }
-    }
-}
