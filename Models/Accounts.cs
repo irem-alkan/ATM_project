65efac3cccb205fb64f9d35e1a16da66ad64598a
SIrem Alkan <alkanirem212@gmail.com>
2024-07-11 09:21:00 +0300

    ...

---
+++ Models/Accounts.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ATMWithdrawalApi.Models
+{
+    public class Accounts
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ID_Customer { get; set; }
+        public string ID_Account { get; set; } = string.Empty;
+        public int Balance { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
